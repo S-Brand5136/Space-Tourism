1f2fc5ed3afd418c2f2b16d504429b059c2867c7
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-12-17 18:05:21 -0700

    Init base pages, Navigation component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../helpers/Home";
+import Destination from "../helpers/Destination";
+import Crew from "../helpers/Crew";
+import Technology from "../helpers/Technology";
+import Navigation from "./Navigation";
+
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navigation />
+      <Router>
+        <Switch>
+          <Route path='/' component={<Home />} />
+          <Route path='/destination' component={<Destination />} />
+          <Route path='/crew' component={<Crew />} />
+          <Route path='/technology' component={<Technology />} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
