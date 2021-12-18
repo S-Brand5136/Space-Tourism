1992f5c3929cac6578e35c0e5f8112be48e59919
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-12-17 18:16:06 -0700

    Fix imports, move main app.js file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Destination from "../pages/Destination";
-import Crew from "../pages/Crew";
-import Technology from "../pages/Technology";
-import Navigation from "./Navigation";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
+import Navigation from "./components/Navigation";
 
 const App = () => {
   return (
     <div className='App'>
       <Navigation />
-      <Router>
-        <Switch>
+      <BrowserRouter>
+        <Routes>
           <Route path='/' component={<Home />} />
           <Route path='/destination' component={<Destination />} />
           <Route path='/crew' component={<Crew />} />
           <Route path='/technology' component={<Technology />} />
-        </Switch>
-      </Router>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
