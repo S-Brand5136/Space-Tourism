12c07a0ca0134193ab4744911145fe3deab83fea
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-12-19 14:08:58 -0700

    Create NavItem component for mobile

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import "./styles/NavItem.scss";
 
-const NavItem = () => {
-  return <div></div>;
+const NavItem = ({ active, url, title, index }) => {
+  let navItemClass = classNames("navItem", {
+    "navItem--active": active,
+  });
+
+  return (
+    <li className={navItemClass}>
+      {window.innerWidth <= 428 ? <span>0{index}</span> : ""}
+      <Link to={url}>{title}</Link>
+    </li>
+  );
 };
 
 export default NavItem;
