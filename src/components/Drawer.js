23acd50dbfe0e894a019771e8199816a6673cbfd
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-12-19 14:08:28 -0700

    Fix syntax errors

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -8,16 +8,17 @@ const Drawer = (props) => {
   return (
     <>
       <Offcanvas
+        backdrop={false}
         className='drawer'
         show={props.show}
         onHide={props.close}
         placement='end'
       >
         <div className='bg--blur'></div>
-        <Offcanvas.Header class='drawer--header'>
+        <Offcanvas.Header className='drawer--header'>
           <Button onClick={props.close}>
             <svg xmlns='http://www.w3.org/2000/svg' width='20' height='21'>
-              <g fill='#D0D6F9' fill-rule='evenodd'>
+              <g fill='#D0D6F9' fillRule='evenodd'>
                 <path d='M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z' />
                 <path d='M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z' />
               </g>
