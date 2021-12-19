a92b2c2cf7ca1e01658bf3558ec6cd1da9300100
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-12-19 14:09:08 -0700

    Create NavItem

--- src/components/NavList.js
+++ src/components/NavList.js
@@ -1,7 +1,20 @@
 import React from "react";
+import NavItem from "./NavItem";
 
 const NavList = (props) => {
-  return <ul></ul>;
+  const navItems = props.urls.map((item, index) => {
+    return (
+      <NavItem
+        key={index}
+        title={item.title}
+        url={item.url}
+        active={item.active}
+        index={index}
+      />
+    );
+  });
+
+  return <ul className='navlist'>{navItems}</ul>;
 };
 
 export default NavList;
