8849fb33fbec06711211af5f86845d6410bf65c8
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-12-20 21:25:02 -0700

    Add markup to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,26 @@
 import React from "react";
+import Button from "../components/Button";
 import "./Home.scss";
+import "../components/styles/Button.scss";
 
 const Home = () => {
-  return <main className='home'></main>;
+  return (
+    <main className='home'>
+      <article>
+        <h4>So, you want to travel to</h4>
+        <h1>Space</h1>
+        <p>
+          Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience!
+        </p>
+      </article>
+      <Button className='btn' explore>
+        Explore
+      </Button>
+    </main>
+  );
 };
 
 export default Home;
