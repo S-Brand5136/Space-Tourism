ed183e7ae62237bdb6e29bb6e9caecf770e9fc60
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-12-21 20:44:13 -0700

    Create Page Header

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,7 +1,13 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import PageHeader from "../components/PageHeader";
+import "./Destination.scss";
 
 const Destination = () => {
-  return <div></div>;
+  return (
+    <main className='destination'>
+      <PageHeader number='01' title='Pick your destination' />
+    </main>
+  );
 };
 
 export default Destination;
