47c79b2f0257a3e30dd8eaf513a2657b4afe63f7
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-12-21 21:11:42 -0700

    Add Page Header, imports and Carousel component

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import PageHeader from "../components/PageHeader";
+import { Carousel } from "react-bootstrap";
+import { request } from "../helpers/request";
 import "./Destination.scss";
 
 const Destination = () => {
   return (
     <main className='destination'>
       <PageHeader number='01' title='Pick your destination' />
+      <Carousel></Carousel>
     </main>
   );
 };
