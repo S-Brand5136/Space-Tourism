29be0134f60c891c157c742e9fb6d21fc06afab5
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-12-22 21:42:43 -0700

    Create Carousel component with images

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -5,10 +5,39 @@ import { request } from "../helpers/request";
 import "./Destination.scss";
 
 const Destination = () => {
+  const [destinations, setDestinations] = useState(null);
+  const [labels, setLabels] = useState(null);
+
+  useEffect(() => {
+    if (destinations === null) {
+      request.get("destinations").then((data) => {
+        setDestinations(data);
+        const labels = data.map((item) => item.name);
+        setLabels(labels);
+      });
+    }
+  }, [destinations, labels]);
+
   return (
     <main className='destination'>
       <PageHeader number='01' title='Pick your destination' />
-      <Carousel></Carousel>
+      <Carousel interval={null} controls={false}>
+        {destinations ? (
+          destinations.map((item) => {
+            return (
+              <Carousel.Item interval={null} key={item.name}>
+                <img
+                  className='carousel-image'
+                  src={process.env.PUBLIC_URL + `${item.images.png}`}
+                  alt={item.name}
+                />
+              </Carousel.Item>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </Carousel>
     </main>
   );
 };
