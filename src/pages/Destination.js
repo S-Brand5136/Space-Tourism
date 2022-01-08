706455f716b796b9645a02687eb3521a3f6c4611
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-07 22:34:21 -0700

    Add buttongGroup, rework Carousel component

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import PageHeader from "../components/PageHeader";
+import ButtonGroup from "../components/ButtonGroup";
 import { Carousel } from "react-bootstrap";
 import { request } from "../helpers/request";
 import "./Destination.scss";
 
 const Destination = () => {
-  const [destinations, setDestinations] = useState(null);
-  const [labels, setLabels] = useState(null);
+  const [destinations, setDestinations] = useState([]);
+  const [labels, setLabels] = useState([]);
+  const [itemNum, setItemNum] = useState(0);
 
   useEffect(() => {
-    if (destinations === null) {
+    if (destinations.length <= 0) {
       request.get("destinations").then((data) => {
         setDestinations(data);
         const labels = data.map((item) => item.name);
@@ -18,26 +20,34 @@ const Destination = () => {
     }
   }, [destinations, labels]);
 
+  const carouselHandler = (num) => {
+    setItemNum(num);
+  };
+
   return (
     <main className='destination'>
-      <PageHeader number='01' title='Pick your destination' />
-      <Carousel interval={null} controls={false}>
-        {destinations ? (
-          destinations.map((item) => {
-            return (
-              <Carousel.Item interval={null} key={item.name}>
-                <img
-                  className='carousel-image'
-                  src={process.env.PUBLIC_URL + `${item.images.png}`}
-                  alt={item.name}
-                />
-              </Carousel.Item>
-            );
-          })
-        ) : (
-          <></>
-        )}
+      <PageHeader number={"01"} title='Pick your destination' />
+      <Carousel
+        interval={null}
+        controls={false}
+        indicators={false}
+        activeIndex={itemNum}
+      >
+        {destinations.map((item) => {
+          return (
+            <Carousel.Item interval={null} key={item.name}>
+              <img
+                className='carousel-image'
+                src={process.env.PUBLIC_URL + `${item.images.png}`}
+                alt={item.name}
+              />
+            </Carousel.Item>
+          );
+        })}
       </Carousel>
+      <div className='indicators'>
+        <ButtonGroup itemArray={labels} handler={carouselHandler} />
+      </div>
     </main>
   );
 };
