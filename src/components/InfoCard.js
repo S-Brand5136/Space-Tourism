8fdc1a22675d86645671f99db229e6b3f5f500df
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-08 21:28:28 -0700

    Break out info for carouselItems into own comp.

---
+++ src/components/InfoCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Text from "./Text";
+
+import "./styles/InfoCard.scss";
+
+const InfoCard = (props) => {
+  return (
+    <section className='info-card'>
+      <div className='top'>
+        <Text tag='h1' heading>
+          {props.item.name}
+        </Text>
+        <Text>{props.item.description}</Text>
+      </div>
+      <div className='bottom'>
+        <Text secondaryHeading tag='h6'>
+          AVG. DISTANCE
+        </Text>
+        <Text primary tag='h3'>
+          {props.item.distance}
+        </Text>
+        <Text secondaryHeading tag='h6'>
+          EST. TRAVEL TIME
+        </Text>
+        <Text primary tag='h3'>
+          {props.item.travel}
+        </Text>
+      </div>
+    </section>
+  );
+};
+
+export default InfoCard;
