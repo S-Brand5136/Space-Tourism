215a5e9b47d658e67dadc04d5ad36a811a4de1b9
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-09 18:28:55 -0700

    Create scss sheets for crew,tech pages

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./Crew.scss";
+
 const Crew = () => {
   return <div></div>;
 };
--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./Technology.scss";
+
 const Technology = () => {
   return <div></div>;
 };
