4f355db81630f819f95116aa867a615842cc9194
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-11 21:08:06 -0700

    Add useEffect with fake GET request

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,8 +1,19 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { request } from "../helpers/request";
 
 import "./styles/Crew.scss";
 
 const Crew = () => {
+  const [crewData, setCrewData] = useState([]);
+
+  useEffect(() => {
+    if (crewData.length <= 0) {
+      request.get("crew").then((data) => {
+        setCrewData(data);
+      });
+    }
+  }, [crewData]);
+
   return <main className='crew'></main>;
 };
 
