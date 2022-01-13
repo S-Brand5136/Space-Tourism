75bc667052bd2916cfb7e537aa0c37d9f3fa465e
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-12 21:57:06 -0700

    add basic JSX for crew page

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
+import { Carousel } from "react-bootstrap";
+import ButtonGroup from "../components/ButtonGroup";
+import InfoCard from "../components/InfoCard";
+import PageHeader from "../components/PageHeader";
 import { request } from "../helpers/request";
 
 import "./styles/Crew.scss";
 
 const Crew = () => {
   const [crewData, setCrewData] = useState([]);
+  const [itemNum, setItemNum] = useState(0);
 
   useEffect(() => {
     if (crewData.length <= 0) {
@@ -14,7 +19,35 @@ const Crew = () => {
     }
   }, [crewData]);
 
-  return <main className='crew'></main>;
+  const carouselHandler = (num) => {
+    setItemNum(num);
+  };
+
+  return (
+    <main className='crew'>
+      <PageHeader number={"02"} title='Meet your crew' />
+      <Carousel
+        interval={null}
+        controls={false}
+        indicators={false}
+        activeIndex={itemNum}
+      >
+        {crewData.map((item) => {
+          return (
+            <Carousel.Item interval={null} key={item.name}>
+              <img
+                className='carousel-image'
+                src={process.env.PUBLIC_URL + `${item.images.png}`}
+                alt=''
+              />
+              <InfoCard item={item} />
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+      <div className='indicators'>{/* <ButtonGroup /> */}</div>
+    </main>
+  );
 };
 
 export default Crew;
