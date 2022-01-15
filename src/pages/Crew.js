4078cff6467380bf647619a5fdecc6896ed41d5b
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-14 23:05:17 -0700

    Add button group, info-card, image container div

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
 import ButtonGroup from "../components/ButtonGroup";
-import InfoCard from "../components/InfoCard";
 import PageHeader from "../components/PageHeader";
+import Text from "../components/Text";
 import { request } from "../helpers/request";
 
 import "./styles/Crew.scss";
@@ -35,17 +35,33 @@ const Crew = () => {
         {crewData.map((item) => {
           return (
             <Carousel.Item interval={null} key={item.name}>
-              <img
-                className='carousel-image'
-                src={process.env.PUBLIC_URL + `${item.images.png}`}
-                alt=''
-              />
-              <InfoCard item={item} />
+              <div className='image-container'>
+                <img
+                  className='carousel-image'
+                  src={process.env.PUBLIC_URL + `${item.images.png}`}
+                  alt=''
+                />
+              </div>
+              <section className='info-card'>
+                <Text secondary secondaryHeading tag='h5'>
+                  {item.role}
+                </Text>
+                <Text secondaryHeading heading tag='h3'>
+                  {item.name}
+                </Text>
+                <Text className='mt-5'>{item.bio}</Text>
+              </section>
             </Carousel.Item>
           );
         })}
+        <div className='indicators'>
+          <ButtonGroup
+            rounded={true}
+            itemArray={["", "", "", ""]}
+            handler={carouselHandler}
+          />
+        </div>
       </Carousel>
-      <div className='indicators'>{/* <ButtonGroup /> */}</div>
     </main>
   );
 };
