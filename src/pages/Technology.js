8321815c374d09bc1328eb0958c2b644d653b33b
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-17 21:40:03 -0700

    Add base styling/JSX/JS

--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -1,9 +1,71 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import PageHeader from "../components/PageHeader";
+import ButtonGroup from "../components/ButtonGroup";
+import { Carousel } from "react-bootstrap";
+import Text from "../components/Text";
 
 import "./styles/Technology.scss";
+import { request } from "../helpers/request";
 
 const Technology = () => {
-  return <main className='tech'></main>;
+  const [data, setData] = useState([]);
+  const labels = [1, 2, 3];
+  const [itemNum, setItemNum] = useState(0);
+
+  useEffect(() => {
+    if (data.length <= 0) {
+      request.get("technology").then((data) => {
+        setData(data);
+        console.log(data);
+      });
+    }
+  }, [data]);
+
+  const carouselHandler = (num) => {
+    setItemNum(num);
+  };
+
+  return (
+    <main className='tech'>
+      <PageHeader number={"03"} title='space launch 101' />
+      <Carousel
+        interval={null}
+        controls={false}
+        indicators={false}
+        activeIndex={itemNum}
+      >
+        {data.map((item) => {
+          return (
+            <Carousel.Item interval={null} key={item.name}>
+              <div className='image-container'>
+                <img
+                  className='carousel-image'
+                  src={process.env.PUBLIC_URL + `${item.images.landscape}`}
+                  alt=''
+                />
+              </div>
+              <section className='info-card'>
+                <Text secondary secondaryHeading tag='h5'>
+                  The Technology...
+                </Text>
+                <Text secondaryHeading heading tag='h3'>
+                  {item.name}
+                </Text>
+                <Text className='mt-5'>{item.description}</Text>
+              </section>
+            </Carousel.Item>
+          );
+        })}
+        <div className='indicators'>
+          <ButtonGroup
+            active={itemNum}
+            itemArray={labels}
+            handler={carouselHandler}
+          />
+        </div>
+      </Carousel>
+    </main>
+  );
 };
 
 export default Technology;
