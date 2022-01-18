996782531c2c7d8868a286971395c657f80a7e27
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-17 20:45:28 -0700

    Pass active prop to buttonGroup

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -58,6 +58,7 @@ const Crew = () => {
           <ButtonGroup
             rounded={true}
             itemArray={["", "", "", ""]}
+            active={itemNum}
             handler={carouselHandler}
           />
         </div>
