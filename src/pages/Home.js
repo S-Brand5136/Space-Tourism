0fb5310fc41700d29c1add294338bd2e5a69ba2f
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-01-22 17:08:11 -0700

    Update Home page to use Text component/styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
+import Text from "../components/Text";
 import "./styles/Home.scss";
 import "../components/styles/Button.scss";
 
@@ -14,14 +15,18 @@ const Home = () => {
   return (
     <main className='home'>
       <article>
-        <h4>So, you want to travel to</h4>
-        <h1>Space</h1>
-        <p>
+        <Text secondaryHeading secondary primary tag='h4'>
+          So, you want to travel to
+        </Text>
+        <Text heading tag='h1'>
+          Space
+        </Text>
+        <Text>
           Let's face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we'll give you a truly out of this world
           experience!
-        </p>
+        </Text>
       </article>
       <Button onClick={clickHandler} className='btn' explore>
         Explore
